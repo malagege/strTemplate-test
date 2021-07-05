4157288f88ef314fed195c1f7d8a1787da450782
malagege <s9714020@gmail.com>
2021-07-06 00:06:16 +0800

    add header,footer

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      strTemplate
+    </a>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
