cbcb394fd0bccdbea4ec0ab38533729bde734b25
malagege <s9714020@gmail.com>
2021-07-27 20:57:02 +0800

    regex global mode replace strTemplate

--- src/strTemplateHelper.js
+++ src/strTemplateHelper.js
@@ -12,7 +12,7 @@ function strTemplate(str, data){
     console.log('hello',Object.keys(data))
     let template = str;
     Object.keys(data).forEach( key => {
-        let paramRegex = new RegExp('{{\\s*' + key +'\\s*}}','i')
+        let paramRegex = new RegExp('{{\\s*' + key +'\\s*}}','g')
         let paramValue = data[key]
         console.log('paramRegex', paramRegex, 'paramValue', paramValue)
         template = template.replace( paramRegex, paramValue ) 
