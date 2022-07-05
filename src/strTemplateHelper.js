24b097c38dc80846e84cec1aeb0f5eae6c192cc6
malagege <s9714020@gmail.com>
2022-07-05 22:19:13 +0800

    新增 pascal 和 camel helper

--- src/strTemplateHelper.js
+++ src/strTemplateHelper.js
@@ -1,6 +1,24 @@
 import * as jsYaml from 'js-yaml'
 import  template7 from 'template7';
 
+
+template7.registerHelper('pascal', function (text, options){
+    if(!!!text){
+        return ""
+    }
+    let firstChar = text.substr(0,1).toUpperCase();
+    return firstChar + text.substr(1);
+});
+
+template7.registerHelper('camel', function (text, options){
+    if(!!!text){
+        return ""
+    }
+    let firstChar = text.substr(0,1).toLowerCase();
+    return firstChar + text.substr(1);
+});
+
+
 function parseYaml(str){
     return jsYaml.load(str)
 }
